d3be69ca52ddb372553391eedac918a361b21f2c
Sujin Sam <sujinsam.dodge@gmail.com>
2021-05-05 09:05:31 +0530

    SS - drumkit site plays sound with keyboard & clicks

--- DiceChallenge/index.js
+++ DiceChallenge/index.js
@@ -2,10 +2,12 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 document
   .querySelector(".img1")
   .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document
   .querySelector(".img2")
   .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerText = "Player 1 wins!!";
 } else if (randomNumber2 > randomNumber1) {
