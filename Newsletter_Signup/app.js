79d280be7f147c87c2b98c6aa271feadf9d854cd
Sujin Sam <sujinsam.dodge@gmail.com>
2021-05-16 12:14:03 +0530

    newsletter app with mailchimp

---
+++ Newsletter_Signup/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const port = 3100;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/signup.html');
+});
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/images', express.static(__dirname + '/images'));
+
+app.post('/', (req, res) => {
+	const data = {
+		members: [
+			{
+				email_address: req.body.email,
+				status: 'subscribed',
+				merge_fields: {
+					FNAME: req.body.firstName,
+					LNAME: req.body.lastName
+				}
+			}
+		]
+	};
+
+	const jsonData = JSON.stringify(data);
+	const url = 'https://us1.api.mailchimp.com/3.0/lists/5cb64a38e2';
+	const options = {
+		method: 'POST',
+		auth: 'key:a59ef55c1df559909be33523d0fc2fb64-us1'
+	};
+
+	const request = https.request(url, options, (api_response) => {
+		api_response.on('data', (data) => {
+			console.log(JSON.parse(data));
+		});
+		console.log(api_response.statusCode);
+		if (api_response.statusCode == 200) {
+			res.sendFile(__dirname + '/success.html');
+		} else {
+			res.sendFile(__dirname + '/failure.html');
+		}
+	});
+
+	request.write(jsonData);
+	request.end();
+});
+
+app.post('/fail', (req, res) => {
+	res.redirect('/');
+});
+
+app.listen(port, () => {
+	console.log('server listening at ' + port);
+});
